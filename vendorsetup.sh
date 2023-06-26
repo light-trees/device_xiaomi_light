2c135f743b6841a500297934caa434b2eb206836
Sushrut1101 <guptasushrut@gmail.com>
2023-06-26 13:44:49 +0530

    light: Move prebuilt kernel to another repo
    Signed-off-by: adarsh956 <adarshsingh8312@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,13 +1,12 @@
 # Remove Existing Repos
 rm -rf vendor/xiaomi/light
-rm -rf kernel/xiaomi/light
 rm -rf hardware/xiaomi
 rm -rf hardware/mediatek
 rm -rf device/mediatek/sepolicy_vndr
 
 # Clone Repos
 git clone https://github.com/Poco-MT6833/vendor_xiaomi_light vendor/xiaomi/light
-git clone https://github.com/Poco-MT6833/kernel_xiaomi_light kernel/xiaomi/light
 git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 git clone https://github.com/Xiaomi-MT6833/hardware_mediatek hardware/mediatek
+git clone https://github.com/Poco-MT6833/device_xiaomi_light-kernel device/xiaomi/light-kernel
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
