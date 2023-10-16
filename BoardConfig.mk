b3629f1c7c74cbdee1c8efd8d872c06414edf47b
Vaisakh Murali <mvaisakh@statixos.com>
2023-10-16 22:30:37 +0530

    light: Switch to AOSP way of segregating variants AOSP suggests using this way [1]
    [1]: https://source.android.com/docs/core/ota/tools#dynamic-fingerprints
    
    Change-Id: Ida6af5b51cbe2edb4af44411456266cbc1af3ea8
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,6 +150,9 @@ TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/props/system_ext.prop
+TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
 
 # Recovery
 BOARD_USES_RECOVERY_AS_BOOT := true
