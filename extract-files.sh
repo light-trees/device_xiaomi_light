6ee31a2b348c0039365dc750458c0d9586b5dee9
SamarV-121 <samarvispute121@pm.me>
2024-01-04 13:46:10 +0530

    light: Link older libutils for PQ HAL
    * Abort message: 'incStrongRequireStrong() called on 0xb400007c5afa7250 which i>
    
    Change-Id: I7ea86d3d7c68ba183b4838cb1f35a9fc58cfbf67

--- extract-files.sh
+++ extract-files.sh
@@ -59,9 +59,9 @@ function blob_fixup {
     vendor/lib64/libwifi-hal-mtk.so)
         "$PATCHELF" --set-soname libwifi-hal-mtk.so "$2"
         ;;
-    vendor/lib/hw/vendor.mediatek.hardware.pq@2.15-impl.so)
+    vendor/lib/hw/vendor.mediatek.hardware.pq@2.14-impl.so)
         ;&
-    vendor/lib64/hw/vendor.mediatek.hardware.pq@2.15-impl.so)
+    vendor/lib64/hw/vendor.mediatek.hardware.pq@2.14-impl.so)
         "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
         ;;
     vendor/bin/mtk_agpsd)
@@ -107,6 +107,8 @@ function blob_fixup {
         vendor/lib64/mediadrm/libwvdrmengine.so)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
             ;;
+        vendor/bin/hw/vendor.mediatek.hardware.pq@2.2-service)
+            "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
     esac
 }
 
