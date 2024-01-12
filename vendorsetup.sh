3112dba690bad8b7e50490da77b88bacf93aad64
Amrito1 <bhabanidas431@gmail.com>
2024-01-12 23:19:37 +0530

    light: Track Vendor From https://github.com/light-trees/vendor_xiaomi_light.git

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,6 @@
-# Remove Existing Repos
-rm -rf vendor/xiaomi/light
-rm -rf hardware/xiaomi
-rm -rf hardware/mediatek
-rm -rf device/mediatek/sepolicy_vndr
 
 # Clone Repos
-git clone https://github.com/Poco-MT6833/vendor_xiaomi_light vendor/xiaomi/light
+git clone https://github.com/light-trees/vendor_xiaomi_light.git vendor/xiaomi/light
 git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 git clone https://github.com/nashc-dev/android_hardware_mediatek hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
