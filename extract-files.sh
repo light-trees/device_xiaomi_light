d7e7e4d7e2006f6455e158514c811bba00a59401
Signed-off-by: snnbyyds <snnbyyds@gmail.com>
2024-01-14 23:39:51 +0530

    light: Patch android.hardware.thermal@2.0-service.mtk to load VNDK …
    …32 libhidlbase
    
    11-04 12:08:19.027  2760  2760 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.thermal@2.0-service.mtk": cannot locate symbol "_ZN7android8String16aSEOS0_" referenced by "/apex/com.android.vndk.v34/lib64/libhidlbase.so"...
    
    Signed-off-by: snnbyyds <snnbyyds@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -113,6 +113,9 @@ function blob_fixup {
         vendor/lib64/mediadrm/libwvdrmengine.so)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
             ;;
+        vendor/bin/hw/android.hardware.thermal@2.0-service.mtk)
+            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
+            ;;
         vendor/bin/hw/vendor.mediatek.hardware.pq@2.2-service)
             "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
     esac
