e3a92605428ec1c9d2b1317e30cd1043d84ce797
Taki <amrito@myyahoo.com>
2024-02-13 13:22:21 +0530

    light: Move Kernel To Another Repo
    * Old Prebuilt Method Is Depreciated , Due To The Switch Of Inline Kernel Building Changes
    
    Signed-off-by: Amrito1 <bhabanidas431@gmail.com>
    Co-Authored-By: ramshell688 <135526213+ramshell688@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,4 @@ git clone https://github.com/light-trees/vendor_xiaomi_light.git vendor/xiaomi/l
 git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 git clone https://github.com/nashc-dev/android_hardware_mediatek hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+git clone https://github.com/light-trees/device_xiaomi_light-kernel.git -b 12 device/xiaomi/light-kernel
